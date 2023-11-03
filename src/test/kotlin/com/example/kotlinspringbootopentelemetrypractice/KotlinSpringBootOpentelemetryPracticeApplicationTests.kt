a614d1ae0a26d39254d5031766dfb9068804ebc6
Msksgm <msksgm3124@gmail.com>
2023-11-03 17:46:36 +0900

    feat: Spring Boot のプロジェクトを新規作成

---
+++ src/test/kotlin/com/example/kotlinspringbootopentelemetrypractice/KotlinSpringBootOpentelemetryPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinspringbootopentelemetrypractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringBootOpentelemetryPracticeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
